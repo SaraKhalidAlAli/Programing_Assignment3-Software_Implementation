8d6c23f7613024956f678db03e2d53a9d379ab22
SaraKhalidAlAli <202206310@zu.ac.ae>
2024-05-02 03:48:11 +0400

    understanding what you I should do in the code by structuring the parts

---
+++ Event.py
@@ -0,0 +1,21 @@
+#importing
+
+
+
+#adding the classes
+
+
+
+#pickling
+
+
+
+#GUI buttons
+
+# Create buttons for user interaction
+
+# Pack buttons into the GUI window
+
+
+
+#displaying the output
