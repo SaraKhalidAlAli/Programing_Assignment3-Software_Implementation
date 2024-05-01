70e729ad9ff4bcffcd79df4dfdecc761a7274975
SaraKhalidAlAli <202206310@zu.ac.ae>
2024-05-02 03:59:10 +0400

    classes

--- Event.py
+++ Event.py
@@ -1,11 +1,18 @@
 #importing
+
 import pickle
 from tkinter import Tk, Button
 
 
 #adding the classes
 
-
+class Employee:
+class Client:
+class Event:
+class Guest:
+class Venue:
+class Supplier:
+class Caterer:
 
 #pickling
 
