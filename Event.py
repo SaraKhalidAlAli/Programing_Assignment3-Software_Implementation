55a177f5067d689a283a9dc859e878b7740aebc7
SaraKhalidAlAli <202206310@zu.ac.ae>
2024-05-02 04:01:29 +0400

    the two functions, save_data() and load_data(), here are responsible for saving and loading data using the pickle for easy data persistence

--- Event.py
+++ Event.py
@@ -15,7 +15,14 @@ class Supplier:
 class Caterer:
 
 #pickling
+def save_data(data, filename):
+    with open(filename, 'wb') as file:
+        pickle.dump(data, file)
 
+
+def load_data(filename):
+    with open(filename, 'rb') as file:
+        return pickle.load(file)
 
 
 #GUI buttons
