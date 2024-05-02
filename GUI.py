de0d971e1523af44c1f83261d878154f32754fcf
SaraKhalidAlAli <202206310@zu.ac.ae>
2024-05-03 02:39:02 +0400

    adding lists and dictionaries of the classes i will implement below:

--- GUI.py
+++ GUI.py
@@ -9,46 +9,32 @@ class MainWindow:
 
         # List to store employee data
         self.employee_list = []
+        # Dictionary to store client data
+        self.client_data = {}
+        # List to store supplier data
+        self.supplier_list = []
+        # List to store supplier data
+        self.venue_list = []
 
         # Buttons for different options
         self.employee_button = tk.Button(master, text="Add Employee", command=self.open_employee_window)
         self.employee_button.grid(row=0, column=0, padx=10, pady=5, sticky="ew")
-
-        # Button to display employee list
         self.display_employee_button = tk.Button(master, text="Display Employees", command=self.display_employees)
         self.display_employee_button.grid(row=0, column=1, padx=10, pady=5, sticky="ew")
 
-        # Button to add client
         self.client_button = tk.Button(master, text="Add Client", command=self.open_client_window)
         self.client_button.grid(row=1, column=0, padx=10, pady=5, sticky="ew")
 
-    def open_employee_window(self):
-        employee_window = tk.Toplevel(self.master)
-        EmployeeWindow(employee_window, self.employee_list)
+        self.guest_button = tk.Button(master, text="Add Guest", command=self.open_guest_window)
+        self.guest_button.grid(row=2, column=0, padx=10, pady=5, sticky="ew")
 
-    def open_client_window(self):
-        client_window = tk.Toplevel(self.master)
-        ClientWindow(client_window)
+        self.supplier_button = tk.Button(master, text="Add Supplier", command=self.open_supplier_window)
+        self.supplier_button.grid(row=3, column=0, padx=10, pady=5, sticky="ew")
 
-    def display_employees(self):
-        # Create a window to display employee list
-        display_window = tk.Toplevel(self.master)
-        EmployeeDisplayWindow(display_window, self.employee_list)
+        # Button to display employee list
+        self.display_employee_button = tk.Button(master, text="Display Employees", command=self.display_employees)
+        self.display_employee_button.grid(row=4, column=0, padx=10, pady=5, sticky="ew")
 
-
-class EmployeeDisplayWindow:
-    def __init__(self, master, employee_list):
-        self.master = master
-        self.master.title("Employee List")
-        self.employee_list = employee_list
-
-        # Create a label to display employee list
-        self.employee_label = tk.Label(master, text="Employee List", font=("Arial", 14, "bold"))
-        self.employee_label.pack()
-
-        # Create a text widget to display employee data
-        self.employee_text = tk.Text(master, height=20, width=50)
-        self.employee_text.pack(pady=10)
-
-        # Insert employee data into text widget
-        self.display_employees()
+        # Status bar
+        self.status_bar = tk.Label(master, text="", bd=1, relief=tk.SUNKEN, anchor=tk.W)
+        self.status_bar.grid(row=5, column=0, sticky=tk.W + tk.E)
